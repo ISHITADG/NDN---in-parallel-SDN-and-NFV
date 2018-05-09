6bfd434b2bcc40fae056483c0552b5a4e07b611f
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-05-08 20:19:14 -0400

    Update 2_installryu.sh

--- 2_installryu.sh
+++ 2_installryu.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #install ryu on controller
-sudo -s
-apt-get update
-apt-get install --yes libxml2-dev libxslt1-dev python-dev python-eventlet python-routes python-webob python-paramiko python-setuptools python-pip
+sudo apt-get update
+sudo apt-get install --yes libxml2-dev libxslt1-dev python-dev python-eventlet python-routes python-webob python-paramiko python-setuptools python-pip
 git clone git://github.com/osrg/ryu.git
 cd ryu
 sudo pip install -r tools/pip-requires
 sudo python setup.py install
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
-pip install oslo.config
-pip install tinyrpc
+sudo pip install oslo.config
+sudo pip install tinyrpc
