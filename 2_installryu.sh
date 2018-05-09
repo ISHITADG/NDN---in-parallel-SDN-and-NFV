17e479f8785e5d4f52ba4e48b5b1720e113720e1
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-05-08 20:13:45 -0400

    step 2

--- 2_installryu.sh
+++ 2_installryu.sh
@@ -1,8 +1,10 @@
+sudo -s
 apt-get update
 apt-get install --yes libxml2-dev libxslt1-dev python-dev python-eventlet python-routes python-webob python-paramiko python-setuptools python-pip
 git clone git://github.com/osrg/ryu.git
 cd ryu
-python ./setup.py install
+sudo pip install -r tools/pip-requires
+sudo python setup.py install
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 pip install oslo.config
 pip install tinyrpc
