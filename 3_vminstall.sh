f2b1ad6f58782df472fcfbe900e1095324d6de3f
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-06-06 18:21:38 -0400

    Delete 3_vminstall.sh

--- 3_vminstall.sh
+++
@@ -1,8 +0,0 @@
-##Run this first: This installs all the dependencies
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install vim
-sudo apt-get purge libvirt-bin
-sudo apt-get install libvirt-bin
-sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
-echo done
