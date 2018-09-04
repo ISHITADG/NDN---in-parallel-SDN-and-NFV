54ab06e02610650e33dc69dd5eeac02f677b685b
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-09-04 09:19:24 -0400

    Update 1_ndn.sh

--- 1_ndn.sh
+++ 1_ndn.sh
@@ -7,10 +7,20 @@ sudo apt-get update
 sudo apt-get install nfd
 sudo apt-get install ndn-cxx
 sudo apt-get install ndn-cxx-dev
+
+#adding security element
+sudo ndnsec-keygen /`whoami` | ndnsec-install-cert -
+sudo mkdir -p /usr/local/etc/ndn/keys
+sudo ndnsec-cert-dump -i /`whoami` > default.ndncert
+sudo mv default.ndncert /usr/local/etc/ndn/keys/default.ndncert
+
+#install ndnperf & tools
 sudo apt-get install ndn-tools
 nfd-start
-#install ndnperf
 git clone https://github.com/Kanemochi/ndnperf.git
 cd ndnperf/c++/server/
 g++ -o ndnperfserver server.cpp -std=c++11 -O2 -lndn-cxx -lboost_system -lboost_filesystem -lboost_thread -lpthread
+
+
+
 echo done
