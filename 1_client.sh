d1a1e17aee837360c9f6a5226cacd9ce075a5828
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-09-25 20:31:08 -0400

    Update 1_client.sh

--- 1_client.sh
+++ 1_client.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install python-pip python-dev build-essential
-sudo pip install httplib2 --upgrade
-sudo pip install numpy scipy
+sudo apt-get install python-httplib2;
+sudo apt-get install python-setuptools;
+sudo apt-get install python-pip python-dev build-essential;
+sudo pip install --upgrade pip;
+sudo pip install numpy scipy;
+sudo pip install sortedcontainers;
 
 #SETUP DOCKER AND OPENVSWITCH
 sudo wget https://raw.githubusercontent.com/ISHITADG/dockerNDN/master/dockovs.sh
