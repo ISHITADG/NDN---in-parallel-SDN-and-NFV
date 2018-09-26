099f21a4561bdb6d29dfabf0f2caa6941570bc10
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-09-26 16:36:19 -0400

    update path

--- 5_qoendn.py
+++ 5_qoendn.py
@@ -24,9 +24,9 @@ bitrate_array =[]
 cnt_of_switches = []
 num_of_rebuffers = []
 spectrum_array = []
-DASH_BUFFER_FILENAME=glob.glob("/mnt/QUIClientServer0/ASTREAM_LOGS/DASH_BUFFER_LOG_2018-09-26.12_13_32.csv")
+DASH_BUFFER_FILENAME=glob.glob("/users/ishitadg/NDN/DASH_BUFFER/DASH_BUFFER*")
 #'<Absolute path of file, e.g /Users/10runs_sara_v*/dash_buffer*/DASH_BUFFER*>'
-SERVER_LOG_FILENAME=glob.glob("/mnt/QUIClientServer0/ASTREAM_LOGS/BOLA_LOG_2018-09-26.12_13_32.csv")
+SERVER_LOG_FILENAME=glob.glob("/users/ishitadg/NDN/DASH_BUFFER/DASH_BUFFER*")
 #'<Absolute path of file, e.g /Users/10runs_sara_v*/dash_buffer*/SERVER_LOG*>'
 
 print(SERVER_LOG_FILENAME)
