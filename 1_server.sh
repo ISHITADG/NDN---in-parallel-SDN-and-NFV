fe91a33d3a1a9dcc861c21e0c56ee75f413e7a1f
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-10-09 11:45:30 -0400

    update with link to new mpd file

--- 1_server.sh
+++ 1_server.sh
@@ -4,7 +4,8 @@ sudo apt-get install vim
 sudo apt-get install tmux
 wget -r --no-parent --reject \"index.html*\" http://www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/
 cd www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/
-wget 'http://emmy10.casa.umass.edu/CNP/BigBuckBunny_2s.mpd'
+#wget 'http://emmy10.casa.umass.edu/CNP/BigBuckBunny_2s.mpd'
+wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/BigBuckBunny_2s.mpd
 sudo apt-get update
 sudo apt-get install apache2
 
