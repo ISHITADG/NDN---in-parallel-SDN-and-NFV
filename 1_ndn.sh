70848fc936dc871b1e5f109960b5c101679fe476
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-10-10 12:50:43 -0400

    Update 1_ndn.sh

--- 1_ndn.sh
+++ 1_ndn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #install all NDN dependencies and tools
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:named-data/ppa
 sudo apt-get update
 sudo apt-get install -y nfd
