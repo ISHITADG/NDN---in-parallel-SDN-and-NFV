53f14090ed32a3cf7395c85ee440fb5e99856270
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-11-04 17:22:20 -0500

    assume yes

--- 1_server.sh
+++ 1_server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #install bigbuckbunny video file
-sudo apt-get install vim
-sudo apt-get install tmux
+sudo apt-get -y install vim
+sudo apt-get -y install tmux
 wget -r --no-parent --reject \"index.html*\" http://www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/
 cd www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/
 #wget 'http://emmy10.casa.umass.edu/CNP/BigBuckBunny_2s.mpd'
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/BigBuckBunny_2s.mpd
 sudo apt-get update
-sudo apt-get install apache2
+sudo apt-get -y install apache2
 
 echo done
