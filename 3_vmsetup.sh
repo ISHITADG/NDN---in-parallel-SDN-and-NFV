6d6f8226ef27734466c4dfa91b53b572023d04ed
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-11-04 17:30:00 -0500

    assume yes

--- 3_vmsetup.sh
+++ 3_vmsetup.sh
@@ -1,10 +1,10 @@
 ##Run this first: This installs all the dependencies
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install vim
+sudo apt-get -y install vim
 sudo apt-get purge libvirt-bin
-sudo apt-get install libvirt-bin
-sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+sudo apt-get -y install libvirt-bin
+sudo apt-get -y install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
 sudo virsh connect qemu:///system
 sudo brctl addbr virbr1
 sudo brctl addbr virbr2
