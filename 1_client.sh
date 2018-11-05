083b6f7d377969f6f007e7e3680c3b542d3f6e00
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-11-05 13:52:15 -0500

    Update 1_client.sh

--- 1_client.sh
+++ 1_client.sh
@@ -22,7 +22,7 @@ bash dockovs.sh
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/Dockerfile;
 
 #build docker ndn docker type
-docker build -t ndndock --build-arg VERSION_CXX=ndn-cxx-0.6.1 --build-arg VERSION_NFD=NFD-0.6.1 .
+sudo docker build -t ndndock --build-arg VERSION_CXX=ndn-cxx-0.6.1 --build-arg VERSION_NFD=NFD-0.6.1 .
 
 #Run NDN docker
 docker run -d --rm --name ndn1 -p 6364:6363 -p 6365:6363/udp ndndock
