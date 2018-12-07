d79d6e94722cbadceab0220bc146dfcf3ddd3887
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-12-07 12:34:28 -0500

    Update 1_server.sh

--- 1_server.sh
+++ 1_server.sh
@@ -4,7 +4,7 @@ cd /users/ishitadg/ndnperf/c++/server;
 sudo apt-get -y install vim
 sudo apt-get -y install tmux
 mkdir www-itec.uni-klu.ac.at;
-mv /proj/wrfhydro-PG0/ftp/* www-itec.uni-klu.ac.at/;
+mv /proj/wrfhydro-PG0/ftp www-itec.uni-klu.ac.at/;
 cd www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/;
 #wget 'http://emmy10.casa.umass.edu/CNP/BigBuckBunny_2s.mpd'
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/BigBuckBunny_2s.mpd
