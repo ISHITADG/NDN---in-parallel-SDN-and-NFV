fe7d26eba51e94b69ac8fb88ac1d5e6bf551bbae
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-12-11 13:03:22 -0500

    Update iprun.sh

--- iprun.sh
+++ iprun.sh
@@ -4,4 +4,5 @@ cd /users/ishitadg/AStream/dist/client; rm dash_client.py;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/dash_client.py;
 for (( i=0; i<10; i++ )); do
   python dash_client.py -m http://10.10.1.1/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s.mpd -p bola &
+  sleep 1;
 done
