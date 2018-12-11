bcf4174fc66afe33489d1ddc00a465b3babca2ff
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-12-11 09:51:37 -0500

    Update 1_server.sh

--- 1_server.sh
+++ 1_server.sh
@@ -8,7 +8,8 @@ cd www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/BigBuckBunny_2s.mpd
 cd /users/ishitadg/ndnperf/c++/server;
 mv /users/ishitadg/www-itec.uni-klu.ac.at/ .;
-sudo apt-get update
-sudo apt-get -y install apache2
+sudo apt-get update;
+sudo apt-get -y install apache2;
+/etc/init.d/apache2 restart;
 
 echo done
