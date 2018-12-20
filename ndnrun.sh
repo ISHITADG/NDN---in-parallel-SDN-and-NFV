01b47b6ab2ca14aad6a2495c4c18ce18cbfa0b22
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-12-20 14:47:55 -0500

    Runs N no. of clients and stores QoE results
    Results in home/NDN directory

--- ndnrun.sh
+++ ndnrun.sh
@@ -20,3 +20,21 @@ done
 for (( i=0; i<$answer; i++ )); do
   docker exec -w /AStream/dist/client ndn$i python /AStream/dist/client/dash_client_udpD.py -m /www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s.mpd -p bola &
 done
+mkdir /users/ishitadg/NDN; mkdir /users/ishitadg/NDN/DASH_BUFFER; mkdir /users/ishitadg/NDN/BOLA_LOG/;
+cd /users/ishitadg/NDN/DASH_BUFFER;rm *;
+for (( i=0; i<$answer; i++ )); do
+  docker cp ndn$i:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;
+  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG$i$'.csv';
+done
+sudo rm -rf ASTREAM_LOGS/;
+cd /users/ishitadg/NDN/BOLA_LOG; rm *;
+for (( i=0; i<$answer; i++ )); do
+  docker cp ndn$i:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;
+  sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG$i$'.csv';
+done
+sudo rm -rf ASTREAM_LOGS/;
+cd /users/ishitadg/; rm NDN/abr*; rm 5_qoendn.py;
+wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/5_qoendn.py;
+python 5_qoendn.py;
+mv abr* NDN/;
+echo done;
