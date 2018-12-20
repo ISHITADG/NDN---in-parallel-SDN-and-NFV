fafa64d37b24aaa9f04f8d9d051742990869ba00
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-12-20 15:19:47 -0500

    Update iprun.sh

--- iprun.sh
+++ iprun.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 read -p "Run how many clients? " answer
+#IP SETUP
 cd /users/ishitadg/AStream/dist/client; rm *.mpd; rm *.txt; rm -rf TEMP*; rm /mnt/QUIClientServer0/ASTREAM_LOGS/*;
 export id=0
+#RUN CLIENT STREAMING
 for (( i=0; i<$answer; i++ )); do
   python dash_client.py -m http://10.10.1.1/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s.mpd -p bola &
   id=`expr $id + 1`
 done
+#COPY RESULTS
 mkdir /users/ishitadg/IP; mkdir /users/ishitadg/IP/DASH_BUFFER; mkdir /users/ishitadg/IP/BOLA_LOG/;
 cd /users/ishitadg/IP/DASH_BUFFER; sudo rm *;cp /mnt/QUIClientServer0/ASTREAM_LOGS/DASH_BUFFER* .;
 cd /users/ishitadg/IP/BOLA_LOG; sudo rm *;cp /mnt/QUIClientServer0/ASTREAM_LOGS/BOLA_LOG* .;
 
-##if IP over Docker client
+##if IP over Docker client : comment previous 2 lines and uncomment next commented lines
 #for (( i=0; i<$answer; i++ )); do
 #  docker exec -w /AStream/dist/client ndn$i python dash_client.py -m http://10.10.1.1/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s.mpd -p bola &
 #done
@@ -27,8 +30,9 @@ cd /users/ishitadg/IP/BOLA_LOG; sudo rm *;cp /mnt/QUIClientServer0/ASTREAM_LOGS/
 #done
 #sudo rm -rf ASTREAM_LOGS/;
 
+#RUN QoE
 cd /users/ishitadg/; rm IP/abr*; rm 5_qoeip.py;
-wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/5_qoeip.py; 
+wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/5_qoeip.py;
 python 5_qoeip.py;
 mv abr* IP/;
 echo done;
