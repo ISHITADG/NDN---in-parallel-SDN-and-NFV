f3788b4f91a43d7a72b8b9c65cd6cf96a4f7d003
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-12-20 16:52:00 -0500

    Update ndnrun.sh

--- ndnrun.sh
+++ ndnrun.sh
@@ -6,6 +6,10 @@ port1=6364
 port2=6365
 ip=173.16.1.
 network=2
+##for ndn over OVS
+#sudo ovs-vsctl del-br ovs-br1
+#sudo ovs-vsctl add-br ovs-br1
+#sudo ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up
 for (( i=0; i<$answer; i++ )); do
   docker kill ndn$i;
   docker run -d --rm --name ndn$i -p $port1:6363 -p $port2:6363/udp ndndock;
@@ -13,7 +17,7 @@ for (( i=0; i<$answer; i++ )); do
   port2=`expr $port2 + 2`
   ovs-docker add-port ovs-br1 eth1 ndn$i --ipaddress="$ip$network$'/24'";
   network=`expr $network + 1`
-  ##for ndn over OVS
+  #sudo ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up
   #docker exec -ti ndn$i nfdc face create udp://173.16.1.1;
   #docker exec -ti ndn$i nfdc route add /edu/umass udp://173.16.1.1;
   #for ndn over ethernet
