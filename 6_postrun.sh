f3e607abc6e8930916de62f3bf52ed0498bcd681
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-12-20 11:11:12 -0500

    Update 6_postrun.sh

--- 6_postrun.sh
+++ 6_postrun.sh
@@ -1,4 +1,4 @@
-##NDN
+########NDN
 #After run
 #Once: 
 mkdir /users/ishitadg/NDN; mkdir /users/ishitadg/NDN/DASH_BUFFER; mkdir /users/ishitadg/NDN/BOLA_LOG/;
@@ -12,14 +12,32 @@ cd /users/ishitadg/; rm NDN/abr*; rm 5_qoendn.py;wget -L https://raw.githubuserc
 #transfer
 scp -r ishitadg@c220g1-030623.wisc.cloudlab.us:NDN ~/ResearchZink/ndn-results/NFV-NDN/512mb\ cache
 
-##IP
+#########IP
 mkdir /users/ishitadg/IP; mkdir /users/ishitadg/IP/DASH_BUFFER; mkdir /users/ishitadg/IP/BOLA_LOG/;
-##
+
+####IF client host
 #Move all dash buffer log:
 cd /users/ishitadg/IP/DASH_BUFFER; sudo rm *;cp /mnt/QUIClientServer0/ASTREAM_LOGS/DASH_BUFFER* .;
 #Move all bola log:
 cd /users/ishitadg/IP/BOLA_LOG; sudo rm *;cp /mnt/QUIClientServer0/ASTREAM_LOGS/BOLA_LOG* .;
+####IF Docker-IP:
+cd /users/ishitadg/IP/DASH_BUFFER; sudo rm *;docker cp ndn0:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG0.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn1:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG1.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn2:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG2.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn3:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG3.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn4:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG4.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn5:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG5.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn6:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG6.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn7:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG7.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn8:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG8.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn9:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG9.csv; sudo rm -rf ASTREAM_LOGS/;
+#Move all bola log:
+cd /users/ishitadg/IP/BOLA_LOG; rm *;docker cp ndn0:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG0.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn1:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG1.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn2:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG2.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn3:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG3.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn4:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG4.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn5:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG5.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn6:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG6.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn7:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG7.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn8:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG8.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn9:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG9.csv; sudo rm -rf ASTREAM_LOGS/;
+
 #run pythonscript on results
 cd /users/ishitadg/; rm IP/abr*; rm 5_qoeip.py; wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/5_qoeip.py; python 5_qoeip.py;mv abr* IP/;
 #transfer
 scp -r ishitadg@c220g1-030623.wisc.cloudlab.us:IP ~/ResearchZink/ndn-results/NFV-IP/
+
+
+#########IP+NDN
+mkdir /users/ishitadg/IPNDN; mkdir /users/ishitadg/IPNDN/DASH_BUFFER; mkdir /users/ishitadg/IPNDN/BOLA_LOG/;
+cd /users/ishitadg/NDN/DASH_BUFFER; sudo rm *;docker cp ndn0:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG0.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn1:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG1.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn2:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG2.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn3:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG3.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn4:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG4.csv; sudo rm -rf ASTREAM_LOGS/;
+cp /mnt/QUIClientServer0/ASTREAM_LOGS/DASH_BUFFER* .;
+cd /users/ishitadg/NDN/BOLA_LOG; rm *;docker cp ndn0:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG0.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn1:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG1.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn2:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG2.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn3:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG3.csv; sudo rm -rf ASTREAM_LOGS/;docker cp ndn4:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG4.csv; sudo rm -rf ASTREAM_LOGS/;
+cp /mnt/QUIClientServer0/ASTREAM_LOGS/BOLA_LOG* .;
+cd /users/ishitadg/; rm IPNDN/abr*; rm 5_qoeipndn.py;wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/5_qoendn.py;python 5_qoeipndn.py;mv abr* IPNDN/;
+scp -r ishitadg@c220g1-030623.wisc.cloudlab.us:IP ~/ResearchZink/ndn-results/NFV-IP-NDN/
+
+
