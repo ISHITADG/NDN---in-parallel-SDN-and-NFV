3f2eba0ab93f1af683d153bbaf6ce10bb79dfdb7
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-01-04 11:39:04 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -131,20 +131,20 @@ class NDNRouter(app_manager.RyuApp):
             actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
             self.add_ipflow(datapath, 3, match, actions)
             #Following for IP Packets
-            match = parser.OFPMatch(in_port=1, dl_type=0x8000)
+            match = parser.OFPMatch(in_port=1, dl_type=0x0800)
             out_port = 4
             actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
             self.add_ipflow(datapath, 1, match, actions)
-            match = parser.OFPMatch(in_port=4, dl_type=0x8000)
+            match = parser.OFPMatch(in_port=4, dl_type=0x0800)
             out_port = 1
             actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
             self.add_ipflow(datapath, 4, match, actions)
 
-            match = parser.OFPMatch(in_port=5, dl_type=0x8000)
+            match = parser.OFPMatch(in_port=5, dl_type=0x0800)
             out_port = 3
             actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
             self.add_ipflow(datapath, 5, match, actions)
-            match = parser.OFPMatch(in_port=3, dl_type=0x8000)
+            match = parser.OFPMatch(in_port=3, dl_type=0x0800)
             out_port = 5
             actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
             self.add_ipflow(datapath, 3, match, actions)
