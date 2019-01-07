c71585746fccbddff9a25fb7ab6a0fbf91492841
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-01-07 15:19:34 -0500

    Update dash_client_udpDod.py

--- dash_client_udpDod.py
+++ dash_client_udpDod.py
@@ -136,7 +136,7 @@ def get_mpd(url):
     #mpd_data = mpd_conn.read()
 
     #connection.close()
-    quic_cmd="/ndnperf/c++/client/bin/ndnperf -p ndn:/edu/umass -d "+url+" -w 16"
+    quic_cmd="/users/ishitadg/ndnperf/c++/client/bin/ndnperf -p ndn:/edu/umass -d "+url+" -w 16"
     config_dash.LOG.info(quic_cmd)
     stream=os.popen(quic_cmd)
     mpd_file = idr+url.split('/')[-1]
@@ -230,13 +230,13 @@ def download_segment_bola(domain, dp_list, segment_number, segment_url, dash_fol
                     '''             
                     chunk_start_time = timeit.default_timer()
 		    segment_url=segment_url.strip("://")
-    		    quic_cmd="/ndnperf/c++/client/bin/ndnperf -p ndn:/edu/umass -d "+segment_url+" -w 16"
+    		    quic_cmd="/users/ishitadg/ndnperf/c++/client/bin/ndnperf -p ndn:/edu/umass -d "+segment_url+" -w 16"
                     stream=os.system(quic_cmd)
     		    timenow = timeit.default_timer()
                     chunk_dl_time = timenow - chunk_start_time
  		    segment_filename=segment_url.split('/')[-1]
                     #segment_file_handle = open(segment_filename, 'wb')
-		    segment_size+=os.stat("/AStream/dist/client/"+segment_filename).st_size 
+		    segment_size+=os.stat("/users/ishitadg/AStream/dist/client/"+segment_filename).st_size 
                     config_dash.LOG.info(segment_size)
                     #segment_file_handle.write(stream)
 
@@ -458,7 +458,7 @@ def start_playback_smart(dp_object, domain, playback_type=None, download=False, 
     video_segment_duration = 2
     bola_init_state = False
     global idr
-    timer_cmd="/ndnperf/c++/client/bin/ndnperf -p ndn:/edu/umass -d ondemand.txt -w 16"
+    timer_cmd="/users/ishitadg/ndnperf/c++/client/bin/ndnperf -p ndn:/edu/umass -d ondemand.txt -w 16"
     stream=os.system(timer_cmd)
     f_timer=open(idr+"ondemand.txt",'r')
     for line in f_timer.readlines():
