841c4199794c94606281e549ef150cc8746db3f6
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-03-30 15:33:15 -0400

    Update 1_server.sh

--- 1_server.sh
+++ 1_server.sh
@@ -2,6 +2,13 @@
 #install bigbuckbunny video file
 sudo apt-get -y install vim
 sudo apt-get -y install tmux
+git clone https://github.com/esnet/iperf.git;
+cd iperf;
+./configure;
+make;
+make install;
+ldconfig;
+iperf3 -v;
 cd /users/ishitadg/ndnperf/c++/server;
 cp -r /proj/wrfhydro-PG0/www-itec.uni-klu.ac.at .;
 ln -s www-itec.uni-klu.ac.at ondemand;
@@ -11,11 +18,6 @@ ndnsec-cert-dump -i /`whoami` > default.ndncert;
 sudo mv default.ndncert /usr/local/etc/ndn/keys/default.ndncert;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
-#wget -r --no-parent --reject \"index.html*\" http://www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/
-#cd www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/;
-#wget 'http://emmy10.casa.umass.edu/CNP/BigBuckBunny_2s.mpd'
-#wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/BigBuckBunny_2s.mpd
-#mv /users/ishitadg/www-itec.uni-klu.ac.at/ .;
 cd /var/www/html;
 sudo apt-get update;
 sudo apt-get -y install apache2;
@@ -24,7 +26,5 @@ cp -r /proj/wrfhydro-PG0/www-itec.uni-klu.ac.at .;
 ln -s www-itec.uni-klu.ac.at livedata;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py
-#sudo route del -net 10.0.0.0 netmask 255.0.0.0
-#sudo ip route add 10.0.0.0/8 via 10.10.1.4
 
 echo done
