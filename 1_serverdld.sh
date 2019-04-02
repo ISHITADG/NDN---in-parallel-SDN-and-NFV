82241454100ad06d8b9a63d89dd765913f00db49
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-04-02 11:54:18 -0400

    Update 1_serverdld.sh

--- 1_serverdld.sh
+++ 1_serverdld.sh
@@ -21,15 +21,15 @@ ndnsec-cert-dump -i /`whoami` > default.ndncert;
 sudo mv default.ndncert /usr/local/etc/ndn/keys/default.ndncert;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
-cd /var/www/html;
+cd /proj/WRFHydro;
+cp -r /users/ishitadg/ndnperf/c++/server/www-itec.uni-klu.ac.at .;
 sudo apt-get update;
 sudo apt-get -y install apache2;
 /etc/init.d/apache2 restart;
+cd /var/www/html;
 cp -r /users/ishitadg/ndnperf/c++/server/www-itec.uni-klu.ac.at .;
 ln -s www-itec.uni-klu.ac.at livedata;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py
-sudo route del -net 10.0.0.0 netmask 255.0.0.0
-sudo ip route add 10.0.0.0/8 via 10.10.1.4
 
 echo done
