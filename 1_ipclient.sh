87e45f6749bccb74128072b2908f65c978498e3f
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-04-05 15:52:55 -0400

    Update 1_ipclient.sh

--- 1_ipclient.sh
+++ 1_ipclient.sh
@@ -18,25 +18,7 @@ make install;
 ldconfig;
 iperf3 -v;
 
-
-#FOR RUNNING ON THE HOST CLIENT
-#install cmake
-cd /users/ishitadg;
-version=3.12;
-build=2;
-mkdir temp;
-cd temp;
-wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz;
-tar -xzvf cmake-$version.$build.tar.gz;
-cd cmake-$version.$build/;
-./bootstrap;
-make -j4;
-sudo make install;
-cmake --version;
-# compile ndnperf client: 
-cd /users/ishitadg/ndnperf/c++/client/;
-cmake . && make;
-#this installs Astreamer on Client host too
+#ASTreamer for Client host too
 cd /users/ishitadg;
 wget -L https://github.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/blob/master/client.zip?raw=true;
 mv client.zip\?raw\=true client.zip;
@@ -45,5 +27,4 @@ unzip client.zip;
 git clone --recursive https://github.com/pari685/AStream.git;
 rm client.zip;
 cd AStream/dist; rm -rf client; mv ../../client/ .;
-
 echo done
