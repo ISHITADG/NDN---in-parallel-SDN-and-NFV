103bcf4ad6cb84b7e765b45be254d6f2a7896303
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-06-03 16:43:44 -0400

    Update ndnlive.sh

--- ndnlive.sh
+++ ndnlive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Run how many clients? " answer
-
+rm result*;
 #RUN CLIENT STREAMING
 for (( i=0; i<$answer; i++ )); do
-  docker exec -w /AStream/dist/client ndn$i taskset 0x00000001 python /AStream/dist/client/dash_client_udpD.py -m /www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s.mpd -p bola &
+  docker exec ndn$i ndnping -i 1 -c 10000 -t /edu/umass > result$i &
 done
