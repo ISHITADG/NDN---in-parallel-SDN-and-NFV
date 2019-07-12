566c7d14755ea0f8d45e749b0ee1b7e790f6c12e
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-07-12 06:33:25 -0400

    Update startdockers.sh

--- startdockers.sh
+++ startdockers.sh
@@ -10,6 +10,3 @@ for (( i=0; i<$answer; i++ )); do
   port1=`expr $port1 + 2`
   port2=`expr $port2 + 2`
 done
-sudo ovs-vsctl del-br ovs-br1
-sudo ovs-vsctl add-br ovs-br1
-sudo ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up
