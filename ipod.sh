45095e7a7a183a416733231fd9208510784e45cb
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-08-07 07:08:09 -0400

    Update ipod.sh

--- ipod.sh
+++ ipod.sh
@@ -5,6 +5,6 @@ cd /users/ishitadg/AStream/dist/client; rm *.mpd; rm *.txt; rm -rf TEMP*; rm /mn
 export id=0
 #RUN CLIENT STREAMING
 for (( i=0; i<$answer; i++ )); do
-  taskset 0x00000001 python dash_client_od.py -m http://10.10.1.2/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s.mpd -p bola &
+  taskset 0x00000001 python dash_client_od.py -m http://10.10.4.2/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s.mpd -p bola &
   id=`expr $id + 1`
 done
