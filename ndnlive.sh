2c39b0417587752614172bd1d6274c95fed9233d
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-08-16 20:38:45 -0400

    Update ndnlive.sh

--- ndnlive.sh
+++ ndnlive.sh
@@ -2,6 +2,7 @@
 read -p "Run how many clients? " answer
 #RUN CLIENT STREAMING
 for (( i=0; i<$answer; i++ )); do
-  docker exec ndn$i ndndump -i eth1 > trace$i;
+  docker exec ndn$i tcpdump -i eth1 udp port 6363 -vv > tcpdump$i &
+  docker exec ndn$i ndndump -i eth1 > trace$i &
   docker exec -w /AStream/dist/client ndn$i taskset 0x00000001 python /AStream/dist/client/dash_client_udpD.py -m /www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s.mpd -p bola &
 done
