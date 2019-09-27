9e929ff94b73b945f49cb22798b0b0f98c44737b
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-09-27 01:38:03 -0400

    Update 1_serverdld.sh

--- 1_serverdld.sh
+++ 1_serverdld.sh
@@ -2,13 +2,13 @@
 #install bigbuckbunny video file
 sudo apt-get -y install vim
 sudo apt-get -y install tmux
-git clone https://github.com/esnet/iperf.git;
-cd iperf;
-./configure;
-make;
-make install;
-ldconfig;
-iperf3 -v;
+#git clone https://github.com/esnet/iperf.git;
+#cd iperf;
+#./configure;
+#make;
+#make install;
+#ldconfig;
+#iperf3 -v;
 cd /users/ishitadg/ndnperf/c++/server;
 wget -r --no-parent --reject \"index.html*\" http://www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/
 cd www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/;
@@ -21,7 +21,7 @@ ndnsec-cert-dump -i /`whoami` > default.ndncert;
 sudo mv default.ndncert /usr/local/etc/ndn/keys/default.ndncert;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
-cd /proj/WRFHydro;
+cd /proj/CDNABRTest/;
 cp -r /users/ishitadg/ndnperf/c++/server/www-itec.uni-klu.ac.at .;
 sudo apt-get update;
 sudo apt-get -y install apache2;
