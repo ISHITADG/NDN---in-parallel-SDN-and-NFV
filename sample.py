b9fa01be7cd8b02259dc44eb05dde0fd211dfcb0
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-10-15 12:44:58 -0400

    Delete sample.py

--- sample.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/python
-from subprocess import Popen, PIPE
-
-quic_cmd="ndnping /edu/umass -c 10"
-log = open('out.txt', 'w')
-log.flush()
-proc = Popen(quic_cmd, stdout=log, stderr=log, shell=True)
