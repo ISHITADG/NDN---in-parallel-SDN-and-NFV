31fc79ad48b8a456660a8bef3b4bf0472c741c1b
Ishita Dasgupta <ishitadg@cs.umass.edu>
2019-10-25 16:50:09 -0400

    Update 1_server.sh

--- 1_server.sh
+++ 1_server.sh
@@ -10,7 +10,7 @@ make install;
 ldconfig;
 iperf3 -v;
 cd /users/ishitadg/ndnperf/c++/server;
-cp -r /proj/WRFHydro/www-itec.uni-klu.ac.at .;
+cp -r /proj/CDNABRTest/www-itec.uni-klu.ac.at .;
 ln -s www-itec.uni-klu.ac.at ondemand;
 ndnsec-keygen /`whoami` | ndnsec-install-cert -;
 sudo mkdir -p /usr/local/etc/ndn/keys;
@@ -23,7 +23,7 @@ sudo apt-get update;
 sudo apt-get -y install apache2;
 /etc/init.d/apache2 restart;
 cd /var/www/html;
-cp -r /proj/WRFHydro/www-itec.uni-klu.ac.at .;
+cp -r /proj/CDNABRTest/www-itec.uni-klu.ac.at .;
 ln -s www-itec.uni-klu.ac.at livedata;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py
