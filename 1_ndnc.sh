73c944c6cf9b451ba28d917ddfdac013eb57082e
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-05-01 10:58:30 -0400

    Update 1_ndnc.sh

--- 1_ndnc.sh
+++ 1_ndnc.sh
@@ -29,23 +29,7 @@ docker image load -i ndndocker.tar
 rm ndndocker.tar;
 
 #FOR RUNNING ON THE HOST CLIENT
-#install cmake
-cd /users/ishitadg;
-version=3.12;
-build=2;
-mkdir temp;
-cd temp;
-wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz;
-tar -xzvf cmake-$version.$build.tar.gz;
-cd cmake-$version.$build/;
-./bootstrap;
-make -j4;
-sudo make install;
-cmake --version;
-# compile ndnperf client: 
-cd /users/ishitadg/ndnperf/c++/client/;
-cmake . && make;
-#this installs Astreamer on Client host too
+#installing Astreamer for test on the host
 cd /users/ishitadg;
 wget -L https://github.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/blob/master/client.zip?raw=true;
 mv client.zip\?raw\=true client.zip;
