0447dbbaa986483e7b7f73f36ae7422a8ff488b0
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-06-15 14:32:00 -0400

    update nfd.conf location corrected

--- setupdockers.sh
+++ setupdockers.sh
@@ -7,10 +7,10 @@ wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV
 for (( i=0; i<$answer; i++ )); do
   #reset nfd config & simple mpd download script copy
   docker cp dash_client_onlympd.py ndn$i:AStream/dist/client/; 
-  docker cp nfd.conf ndn$i:/usr/local/etc/ndn/nfd.conf;
+  docker cp nfd.conf ndn$i:/etc/ndn/nfd.conf;
   #restart nfd in dockers
-  docker exec -ti ndn$i systemctl stop nfd;
-  docker exec -ti ndn$i start nfd;
+  docker exec -ti ndn$i sudo systemctl stop nfd;
+  docker exec -ti ndn$i sudo systemctl start nfd;
   docker exec -ti ndn$i nfdc route add /edu/umass nexthop 257;
   #docker exec -ti ndn$i nfdc face create udp://173.16.1.1;
   #docker exec -ti ndn$i nfdc route add /edu/umass udp://173.16.1.1;
