745c8c62bbe84327871978635f224cd37fc9de8b
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-06-15 13:40:28 -0400

    script to start nfd before final streaming script

--- setupdockers.sh
+++ setupdockers.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 read -p "Run how many clients? " answer
 
-#NDN Docker setup
-port1=6364
-port2=6365
-ip=173.16.1.
-network=2
+wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/nfd.conf;
+wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/dash_client_onlympd.py;
 ##for ndn over OVS
-sudo ovs-vsctl del-br ovs-br1
-sudo ovs-vsctl add-br ovs-br1
-sudo ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up
 for (( i=0; i<$answer; i++ )); do
-  docker kill ndn$i;
-  docker run -d --rm --name ndn$i -p $port1:6363 -p $port2:6363/udp ndndock;
-  port1=`expr $port1 + 2`
-  port2=`expr $port2 + 2`
-  ovs-docker add-port ovs-br1 eth1 ndn$i --ipaddress=$ip$network$"/24";
-  network=`expr $network + 1`
-  sudo ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up
-  docker exec -ti ndn$i nfdc face create udp://173.16.1.1;
-  docker exec -ti ndn$i nfdc route add /edu/umass udp://173.16.1.1;
-  #for ndn over ethernet
-  #docker exec -ti ndn$i nfdc route add /edu/umass nexthop 256
+  #reset nfd config & simple mpd download script copy
+  docker cp dash_client_onlympd.py ndn$i:AStream/dist/client/; 
+  docker cp nfd.conf ndn$i:/usr/local/etc/ndn/nfd.conf;
+  #restart nfd in dockers
+  docker exec -ti ndn$i systemctl stop nfd;
+  docker exec -ti ndn$i start nfd;
+  docker exec -ti ndn$i nfdc route add /edu/umass nexthop 257;
+  #docker exec -ti ndn$i nfdc face create udp://173.16.1.1;
+  #docker exec -ti ndn$i nfdc route add /edu/umass udp://173.16.1.1;
 done
