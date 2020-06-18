f17fe9e3aa18504f644b0a4962d08297488f5370
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-06-18 13:41:27 -0400

    removed default gateway

--- startdockermcv2.sh
+++ startdockermcv2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Run how many clients? " answer
 #setup mcvlan network 
-docker network create -d macvlan -o parent=enp5s0f0  --subnet 10.10.2.0/24 --gateway 0.0.0.0  --ip-range 10.10.2.10/27 mymcvn2
+docker network create -d macvlan -o parent=enp5s0f0  --subnet 10.10.2.0/24 --ip-range 10.10.2.10/27 mymcvn2
 #NDN Docker setup
 docker rm -f $(docker ps -aq)
 port1=6364
