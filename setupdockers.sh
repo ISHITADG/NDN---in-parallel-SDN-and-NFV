64d6b3ee78ad869ec56207c92f8f9a001c6ae85e
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-06-18 14:16:35 -0400

    update 256 as the route from ether eth0

--- setupdockers.sh
+++ setupdockers.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 read -p "Run how many clients? " answer
-
+rm nfd.conf*;
+rm dash_client_onlympd.py*;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/nfd.conf;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/dash_client_onlympd.py;
 ##for ndn over OVS
 for (( i=0; i<$answer; i++ )); do
   #reset nfd config & simple mpd download script copy
-  docker cp dash_client_onlympd.py ndn$i:AStream/dist/client/; 
+  docker cp dash_client_onlympd.py ndn$i:AStream/dist/client/;
   docker cp nfd.conf ndn$i:/etc/ndn/nfd.conf;
   #restart nfd in dockers
   docker exec -ti ndn$i sudo systemctl stop nfd;
   docker exec -ti ndn$i sudo systemctl start nfd;
-  docker exec -ti ndn$i nfdc route add /edu/umass nexthop 257;
+  docker exec -ti ndn$i nfdc route add /edu/umass nexthop 256;
   #docker exec -ti ndn$i nfdc face create udp://173.16.1.1;
   #docker exec -ti ndn$i nfdc route add /edu/umass udp://173.16.1.1;
 done
