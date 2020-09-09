da288ba7ae4eba2a1517a2548643459258ec9948
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-09-09 17:05:13 -0400

    install data for http transfer
    This only starts apache server & links data to be downloaded

--- 1_server.sh
+++ 1_server.sh
@@ -9,22 +9,15 @@ make;
 make install;
 ldconfig;
 iperf3 -v;
-cd /users/ishitadg/ndnperf/c++/server;
-cp -r /proj/CDNABRTest/www-itec.uni-klu.ac.at .;
-ln -s www-itec.uni-klu.ac.at ondemand;
-ndnsec-keygen /`whoami` | ndnsec-install-cert -;
-sudo mkdir -p /usr/local/etc/ndn/keys;
-ndnsec-cert-dump -i /`whoami` > default.ndncert;
-sudo mv default.ndncert /usr/local/etc/ndn/keys/default.ndncert;
-wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py;
-wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
 
+#install data to be downloaded for IP
 sudo apt-get update;
 sudo apt-get -y install apache2;
 /etc/init.d/apache2 restart;
 cd /var/www/html;
-cp -r /proj/CDNABRTest/www-itec.uni-klu.ac.at .;
-ln -s www-itec.uni-klu.ac.at livedata;
+#cp -r /proj/CDNABRTest/www-itec.uni-klu.ac.at .;
+#ln -s www-itec.uni-klu.ac.at livedata;
+ln -s /proj/CDNABRTest/www-itec.uni-klu.ac.at livedata;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py
 
