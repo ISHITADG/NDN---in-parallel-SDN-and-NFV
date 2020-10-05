cfbc8c937076b436670dc4fcf16fca6b478bc182
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-10-05 15:29:00 -0400

    Update setupdockers.sh

--- setupdockers.sh
+++ setupdockers.sh
@@ -2,17 +2,11 @@
 read -p "Run how many clients? " answer
 ##for ndn over OVS
 for (( i=0; i<$answer; i++ )); do
-  #reset nfd config & simple mpd download script copy
-  docker cp dash_client_onlympd.py ndn$i:AStream/dist/client/;
-  docker cp nfd.conf ndn$i:/etc/ndn/nfd.conf;
-  #restart nfd in dockers
+  #restart nfd & ndn-py-repo in dockers
   docker exec -ti ndn$i sudo systemctl stop nfd;
   docker exec -ti ndn$i sudo systemctl start nfd;
   docker exec -ti ndn$i nfdc route add /edu/umass nexthop 256;
-  docker exec -ti ndn$i mv ndn-python-repo AStream/dist/client;
-  docker exec -ti ndn$i cd AStream/dist/client/ndn-python-repo/ndn-python-repo;
-  docker exec -ti ndn$i systemctl start ndn-python-repo;
-  
+  docker exec -w /ndn-python-repo/ndn-python-repo ndn$i systemctl start ndn-python-repo;
   #docker exec -ti ndn$i nfdc face create udp://173.16.1.1;
   #docker exec -ti ndn$i nfdc route add /edu/umass udp://173.16.1.1;
 done
