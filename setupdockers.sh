28508955da458c2e385e8f597dc784d5f5c3f54f
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-10-04 23:49:57 -0400

    update with npr

--- setupdockers.sh
+++ setupdockers.sh
@@ -9,6 +9,10 @@ for (( i=0; i<$answer; i++ )); do
   docker exec -ti ndn$i sudo systemctl stop nfd;
   docker exec -ti ndn$i sudo systemctl start nfd;
   docker exec -ti ndn$i nfdc route add /edu/umass nexthop 256;
+  docker exec -ti ndn$i mv ndn-python-repo AStream/dist/client;
+  docker exec -ti ndn$i cd AStream/dist/client/ndn-python-repo/ndn-python-repo;
+  docker exec -ti ndn$i systemctl start ndn-python-repo;
+  
   #docker exec -ti ndn$i nfdc face create udp://173.16.1.1;
   #docker exec -ti ndn$i nfdc route add /edu/umass udp://173.16.1.1;
 done
