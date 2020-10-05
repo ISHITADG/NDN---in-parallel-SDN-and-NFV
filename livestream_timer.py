05cbc7804c9e272d10d57f11ca23bd49b1a27b29
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-10-05 15:51:26 -0400

    Update livestream_timer.py

--- livestream_timer.py
+++ livestream_timer.py
@@ -5,6 +5,4 @@ while True:
 	with open("ishita.txt", mode='w') as file:
                 file.write('%s\n' %(timer/2))
         timer+=2
-        python3 putfile.py -r bigbuckbunny --register_prefix /edu/umass -f ishita.txt -n /edu/umass/ishita.txt
-	timer+=2
 	time.sleep(2)
