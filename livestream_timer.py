b387221b6fe41b4bb14ce7da489567f73e9b394c
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-10-05 15:48:06 -0400

    updte and upload to sqlite db

--- livestream_timer.py
+++ livestream_timer.py
@@ -2,7 +2,9 @@ import datetime
 import time
 timer=0
 while True:
-	with open("livestream.txt", mode='w') as file:
-    		file.write('%s\n' %(timer/2))
+	with open("ishita.txt", mode='w') as file:
+                file.write('%s\n' %(timer/2))
+        timer+=2
+        python3 putfile.py -r bigbuckbunny --register_prefix /edu/umass -f ishita.txt -n /edu/umass/ishita.txt
 	timer+=2
 	time.sleep(2)
