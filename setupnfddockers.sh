1e03c3b405eac6f7a9c3d1945d3ee492016a86b0
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-12-15 17:33:39 -0500

    Update and rename setupdockers.sh to setupnfddockers.sh

--- setupnfddockers.sh
+++ setupnfddockers.sh
@@ -6,7 +6,4 @@ for (( i=0; i<$answer; i++ )); do
   docker exec -ti ndn$i sudo systemctl stop nfd;
   docker exec -ti ndn$i sudo systemctl start nfd;
   docker exec -ti ndn$i nfdc route add /edu/umass nexthop 256;
-  docker exec -w /ndn-python-repo/ndn_python_repo ndn$i systemctl start ndn-python-repo;
-  #docker exec -ti ndn$i nfdc face create udp://173.16.1.1;
-  #docker exec -ti ndn$i nfdc route add /edu/umass udp://173.16.1.1;
 done
