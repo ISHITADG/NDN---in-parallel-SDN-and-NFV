4527f98e11b0e288c8e18af8ae762e052f58f3f7
Ishita Dasgupta <ishitadg@cs.umass.edu>
2021-01-03 16:11:17 -0500

    turn off upload

--- 1_server.sh
+++ 1_server.sh
@@ -15,9 +15,9 @@ sudo apt-get update;
 sudo apt-get -y install apache2;
 /etc/init.d/apache2 restart;
 cd /var/www/html;
-#cp -r /proj/CDNABRTest/www-itec.uni-klu.ac.at .;
-#ln -s www-itec.uni-klu.ac.at livedata;
-ln -s /proj/CDNABRTest/www-itec.uni-klu.ac.at livedata;
+cp -r /proj/CDNABRTest/www-itec.uni-klu.ac.at .;
+ln -s www-itec.uni-klu.ac.at livedata;
+#ln -s /proj/CDNABRTest/www-itec.uni-klu.ac.at livedata;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py
 
@@ -25,13 +25,13 @@ wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV
 cd /users/ishitadg/ndn-python-repo/examples/
 #cp -r /proj/CDNABRTest/www-itec.uni-klu.ac.at .;
 #ln -s www-itec.uni-klu.ac.at ondemand;
-ln -s /proj/CDNABRTest/www-itec.uni-klu.ac.at www-itec.uni-klu.ac.at;
-ln -s /proj/CDNABRTest/www-itec.uni-klu.ac.at ondemand;
+#ln -s /proj/CDNABRTest/www-itec.uni-klu.ac.at www-itec.uni-klu.ac.at;
+#ln -s /proj/CDNABRTest/www-itec.uni-klu.ac.at ondemand;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/upload.sh; 
 echo "BEGIN SERVER DATABASE UPLOAD"
 sleep 3
-bash upload.sh
+#bash upload.sh
 
 echo done
