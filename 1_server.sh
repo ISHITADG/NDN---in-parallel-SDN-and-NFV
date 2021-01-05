cfae5e18476581ba6d0cccb05e2df7ce24adf306
Ishita Dasgupta <ishitadg@cs.umass.edu>
2021-01-05 13:32:16 -0500

    Update 1_server.sh

--- 1_server.sh
+++ 1_server.sh
@@ -30,8 +30,9 @@ cd /users/ishitadg/ndn-python-repo/examples/
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/livestream_timer.py;
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/ondemand.py; 
 wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/upload.sh; 
-echo "BEGIN SERVER DATABASE UPLOAD"
-sleep 3
+echo "BEGIN SERVER DATABASE UPLOAD or DATABASE TRASNFER"
+rm /root/.ndn/ndn-python-repo/sqlite3.db;
+cp -r /proj/CDNABRTest/sqlite3.db /root/.ndn/ndn-python-repo/sqlite3.db;
 #bash upload.sh
 
 echo done
