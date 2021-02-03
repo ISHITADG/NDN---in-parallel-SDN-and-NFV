74888644efb32ea8d0de8259ac900c70680c5e9e
Ishita Dasgupta <ishitadg@cs.umass.edu>
2021-02-03 17:06:03 -0500

    add resetting journalct logs

--- setupnfd.sh
+++ setupnfd.sh
@@ -2,6 +2,10 @@
 read -p "Run how many clients? " answer
 ##for ndn over OVS
 for (( i=0; i<$answer; i++ )); do
+  #reset journalct rate limiting off 
+  docker exec -ti ndn$i wget https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/journald.conf;
+  docker exec -ti ndn$i mv journald.conf /etc/systemd/;
+  docker exec -ti ndn$i systemctl restart systemd-journald;
   #restart nfd & ndn-py-repo in dockers
   docker exec -ti ndn$i sudo systemctl stop nfd;
   docker exec -ti ndn$i sudo systemctl start nfd;
