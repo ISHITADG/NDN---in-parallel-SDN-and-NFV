c91835366e9ae099d34e2ca9fe3ea434114affe4
Ishita Dasgupta <ishitadg@cs.umass.edu>
2021-02-08 13:20:01 -0500

    add tcpdump tracing on containers

--- ndnlive.sh
+++ ndnlive.sh
@@ -2,7 +2,7 @@
 read -p "Run how many clients? " answer
 #RUN CLIENT STREAMING
 for (( i=0; i<$answer; i++ )); do
-  #docker exec ndn$i ndndump -i eth1 > trace$i &
+  docker exec ndn$i tcpdump -i eth0 -w $i$".pcap" &
   docker exec ndn$i rm /mnt/QUIClientServer0/ASTREAM_LOGS/*;
   docker exec -w /ndn-python-repo/examples/AStream/dist/client ndn$i taskset 0x00000001 python dashrepocat.py -m BigBuckBunny2s.mpd -p bola &
 done
