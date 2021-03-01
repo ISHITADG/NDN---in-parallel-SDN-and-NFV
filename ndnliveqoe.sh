4dc25f5d7fdeef1e4910bf7e86ebc2a3b9c799c9
Ishita Dasgupta <ishitadg@cs.umass.edu>
2021-03-01 01:49:28 -0500

    Update ndnliveqoe.sh

--- ndnliveqoe.sh
+++ ndnliveqoe.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-read -p "Run how many clients? " answer
+#read -p "Run how many clients? " answer
+answer=10
 #COPY RESULTS
 mkdir -p /users/ishitadg/NDN; mkdir -p /users/ishitadg/NDN/DASH_BUFFER; mkdir -p /users/ishitadg/NDN/BOLA_LOG/;
-cd /users/ishitadg/NDN/DASH_BUFFER;rm *;
+cd /users/ishitadg/NDN/DASH_BUFFER;rm -rf *;
 for (( i=0; i<$answer; i++ )); do
   docker cp ndn$i:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;
   mv ASTREAM_LOGS/DASH_BUFFER* DASH_BUFFER_LOG$i$'.csv';
 done
-sudo rm -rf ASTREAM_LOGS/;
-cd /users/ishitadg/NDN/BOLA_LOG; rm *;
+rm -rf ASTREAM_LOGS;
+cd /users/ishitadg/NDN/BOLA_LOG; rm -rf *;
 for (( i=0; i<$answer; i++ )); do
   docker cp ndn$i:/mnt/QUIClientServer0/ASTREAM_LOGS/ .;
-  sudo  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG$i$'.csv';
+  mv ASTREAM_LOGS/BOLA_LOG* BOLA_LOG$i$'.csv';
 done
-sudo rm -rf ASTREAM_LOGS/;
+rm -rf ASTREAM_LOGS/;
 
 #RUN QOE
 cd /users/ishitadg/; rm NDN/abr*;
+#rm 5_qoendn.py;
+#wget -L https://raw.githubusercontent.com/ISHITADG/NDN---in-parallel-SDN-and-NFV/master/5_qoendn.py;
 python 5_qoendn.py;
 mv abr* NDN/;
 echo done;
