9af360645439ea516a19fdeba5a9cab5742510f1
Ishita Dasgupta <ishitadg@cs.umass.edu>
2021-03-01 16:10:46 -0500

    mask journald

--- setupnfd.sh
+++ setupnfd.sh
@@ -3,10 +3,11 @@ read -p "Run how many clients? " answer
 ##for ndn over OVS
 for (( i=0; i<$answer; i++ )); do
   #reset journalct rate limiting off
-  docker cp journald.conf ndn$i:/etc/systemd/;
-  docker exec -ti ndn$i systemctl restart systemd-journald;
+  #docker cp journald.conf ndn$i:/etc/systemd/;
+  #docker exec -ti ndn$i systemctl restart systemd-journald;
   #restart nfd & ndn-py-repo in dockers
   docker exec -ti ndn$i sudo systemctl stop nfd;
   docker exec -ti ndn$i sudo systemctl start nfd;
   docker exec -ti ndn$i nfdc route add /edu/umass nexthop 256;
+  docker exec -ti ndn$i systemctl mask systemd-journald.service;
 done
