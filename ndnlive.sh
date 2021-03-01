32ea7cafcabfc5e6101f64ec5319349dd6a5a5fa
Ishita Dasgupta <ishitadg@cs.umass.edu>
2021-03-01 17:13:46 -0500

    Update ndnlive.sh

--- ndnlive.sh
+++ ndnlive.sh
@@ -3,5 +3,5 @@ read -p "Run how many clients? " answer
 #RUN CLIENT STREAMING
 for (( i=0; i<$answer; i++ )); do
   #docker exec ndn$i rm /mnt/QUIClientServer0/ASTREAM_LOGS/*;
-  docker exec -w /ndn-python-repo/examples/AStream/dist/client ndn$i taskset 0x00000001 python dashrepocat.py -m BigBuckBunny2s.mpd -p bola &
+  docker exec -w /AStream/dist/client ndn$i taskset 0x00000001 python dashrepocat.py -m BigBuckBunny2s.mpd -p bola &
 done
